cec8d65dfd9a3c85216d83d1d5bae256b44eaf0d
njk345 <njk@Nicks-MacBook-Retina.local>
2016-04-02 21:04:33 -0400

    Preliminary Tabs

--- HackBCA_App/www/js/app.js
+++ HackBCA_App/www/js/app.js
@@ -22,3 +22,88 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+  // setup an abstract state for the tabs directive
+  .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+  })
+
+  // Each tab has its own nav history stack:
+
+  /*
+  .state('tab.sync', {
+    url: '/sync',
+    views: {
+      'tab-sync': {
+        templateUrl: 'templates/sync.html',
+        controller: 'SyncCtrl'
+      }
+    }
+  })
+
+  // Database stuff
+  .state('tab.data', {
+    url: '/data',
+    views: {
+      'tab-data': {
+        templateUrl: 'templates/data.html',
+        controller: 'DataCtrl'
+      }
+    }
+  })
+
+  .state('tab.team', {
+    url: '/data/:teamNum',
+    views: {
+      'tab-data': {
+        templateUrl: 'templates/team.html',
+        controller: 'TeamCtrl'
+      }
+    }
+  })
+
+  .state('tab.rankings', {
+    url: '/rankings',
+    views: {
+      'tab-rankings': {
+        templateUrl: 'templates/rankings.html',
+        controller: 'RankingCtrl'
+      }
+    }
+  })*/
+
+  .state('tab.submitSomething', {
+    url: '/submit/:teamNum',
+    views: {
+      'tab-submit': {
+        templateUrl: 'templates/submit.html',
+        controller: 'SubmitCtrl'
+      }
+    }
+  })
+
+  .state('tab.submit', {
+    url: '/submit',
+    views: {
+      'tab-submit': {
+        templateUrl: 'templates/submit.html',
+        controller: 'SubmitCtrl'
+      }
+    }
+  });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/submit');
+
+});
+
