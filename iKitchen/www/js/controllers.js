1a37b1b72dbd271a48f803b5439f741e5db7fa6e
vaedprasad <VaedPrasad@Vaeds-MacBook-Pro-3.local>
2016-04-03 02:39:20 -0400

    Fixing your shit keirstied

--- iKitchen/www/js/controllers.js
+++ iKitchen/www/js/controllers.js
@@ -26,6 +26,30 @@ angular.module('starter.controllers', [])
     enableFriends: true
   };
 });
+module.controller('BarcodeCtrl', function($scope, $cordovaBarcodeScanner) {
+
+  document.addEventListener("deviceready", function () {
+
+    $cordovaBarcodeScanner
+      .scan()
+      .then(function(barcodeData) {
+        // Success! Barcode data is here
+      }, function(error) {
+        // An error occurred
+      });
+
+
+    // NOTE: encoding not functioning yet
+    $cordovaBarcodeScanner
+      .encode(BarcodeScanner.Encode.TEXT_TYPE, "http://www.nytimes.com")
+      .then(function(success) {
+        // Success!
+      }, function(error) {
+        // An error occurred
+      });
+
+  }, false);
+});
 
 /*.controller("BarcodeCtrl", function($scope, $cordovaBarcodeScanner) {
    
