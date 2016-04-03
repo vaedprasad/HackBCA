f0869d108e5342afc470e4970505c68868c7c00c
vaedprasad <VaedPrasad@Vaeds-MacBook-Pro-3.local>
2016-04-02 21:04:53 -0400

    g
    g

--- HackBCA_App/www/js/app.js
+++ HackBCA_App/www/js/app.js
@@ -3,21 +3,21 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
       cordova.plugins.Keyboard.disableScroll(true);
+
     }
-    if(window.StatusBar) {
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
   });
@@ -32,7 +32,11 @@ angular.module('starter', ['ionic'])
   $stateProvider
 
   // setup an abstract state for the tabs directive
+<<<<<<< Updated upstream
   .state('tab', {
+=======
+    .state('tab', {
+>>>>>>> Stashed changes
     url: '/tab',
     abstract: true,
     templateUrl: 'templates/tabs.html'
@@ -40,6 +44,7 @@ angular.module('starter', ['ionic'])
 
   // Each tab has its own nav history stack:
 
+<<<<<<< Updated upstream
   /*
   .state('tab.sync', {
     url: '/sync',
@@ -47,10 +52,19 @@ angular.module('starter', ['ionic'])
       'tab-sync': {
         templateUrl: 'templates/sync.html',
         controller: 'SyncCtrl'
+=======
+  .state('tab.dash', {
+    url: '/dash',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/tab-dash.html',
+        controller: 'DashCtrl'
+>>>>>>> Stashed changes
       }
     }
   })
 
+<<<<<<< Updated upstream
   // Database stuff
   .state('tab.data', {
     url: '/data',
@@ -98,12 +112,45 @@ angular.module('starter', ['ionic'])
       'tab-submit': {
         templateUrl: 'templates/submit.html',
         controller: 'SubmitCtrl'
+=======
+  .state('tab.chats', {
+      url: '/chats',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/tab-chats.html',
+          controller: 'ChatsCtrl'
+        }
+      }
+    })
+    .state('tab.chat-detail', {
+      url: '/chats/:chatId',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/chat-detail.html',
+          controller: 'ChatDetailCtrl'
+        }
+      }
+    })
+
+  .state('tab.account', {
+    url: '/account',
+    views: {
+      'tab-account': {
+        templateUrl: 'templates/tab-account.html',
+        controller: 'AccountCtrl'
+>>>>>>> Stashed changes
       }
     }
   });
 
   // if none of the above states are matched, use this as the fallback
+<<<<<<< Updated upstream
   $urlRouterProvider.otherwise('/tab/submit');
 
 });
 
+=======
+  $urlRouterProvider.otherwise('/tab/dash');
+
+});
+>>>>>>> Stashed changes
